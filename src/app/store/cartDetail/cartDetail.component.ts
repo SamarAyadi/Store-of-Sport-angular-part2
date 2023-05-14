ac8b8c0ffdd5e8068d7c5be057125f05f2aa9e81
Samar AYADI <ayadisamardev@gmail.com>
2023-05-14 14:15:54 +0100

    Secend step

---
+++ src/app/store/cartDetail/cartDetail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { Cart } from "src/app/model/cart.model";
+
+@Component({
+    templateUrl: 'cartDetail.component.html'
+})
+
+export class CartDetailComponent {
+
+    constructor(public cart: Cart){}
+}
---
+++ src/app/store/store.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ModelModule } from '../model/model.module';
+import { StoreComponent } from './store.component';
+import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from './cartSummary/cartSummary.component';
+import { CartDetailComponent } from './cartDetail/cartDetail.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+    imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
+    declarations: [
+        StoreComponent, 
+        CounterDirective, 
+        CartSummaryComponent,
+        CartDetailComponent,
+        CheckoutComponent,
+    ],
+    exports: [StoreComponent, CartDetailComponent, CheckoutComponent,],
+})
+
+export class StoreModule {}
